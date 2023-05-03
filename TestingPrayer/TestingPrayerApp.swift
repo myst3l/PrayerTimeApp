a8f525e515994189b1239461c18d35607d8c80fa
myst3l <karakumayl@gmail.com>
2023-05-03 19:00:45 -0400

    Initial Commit

---
+++ TestingPrayer/TestingPrayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingPrayerApp.swift
+//  TestingPrayer
+//
+//  Created by Kumayl Kara on 2023-05-03.
+//
+
+import SwiftUI
+
+@main
+struct TestingPrayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
